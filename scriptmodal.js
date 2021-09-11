30b19b9bd95c8133dab4d613038526bc56e53800
jehu <alanis199426@hotmail.com>
2021-09-10 23:00:28 -0500

    v2

--- scriptmodal.js
+++ scriptmodal.js
@@ -200,3 +200,33 @@ window.onclick = function(event) {
     modal7.style.display = "none";
   }
 }
+
+
+
+
+var modal8 = document.getElementById("myModal8");
+
+// Get the button that opens the modal
+var btn8 = document.getElementById("myBtn8");
+
+var span8 = document.getElementsByClassName("close8")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span8.onclick = function() {
+    modal8.style.display = "none";
+    
+  }
+
+// When the user clicks the button, open the modal 
+btn8.onclick = function() {
+  modal8.style.display = "block";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal8) {
+    modal8.style.display = "none";
+  }
+}
